902c904ddf12647a70ffc68b87ce540577933d33
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2015-06-13 01:19:04 +0300

    PEP-8

--- src/algorithmbezier.py
+++ src/algorithmbezier.py
@@ -1,8 +1,10 @@
 from point import point
 
+# Vector's radius
 def sumspoint(a, b, t):
     return point(b.x * t + a.x * (1 - t),
                  b.y * t + a.y * (1 - t))
+
 
 def bezier(P, t):
     if len(P) == 1:
@@ -12,16 +14,12 @@ def bezier(P, t):
         for i in range(len(P) - 1):
             newP.append(sumspoint(P[i], P[i + 1], t))
         return bezier(newP, t)
-        
-        
+    
+  
 def algorithmbezier(P):
     points = []
     for i in range(100):
         t = i / 100
         points.append(bezier(P, t))
-        
+
     return points
-    
-            
-        
-        
