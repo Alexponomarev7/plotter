2d6c4adb1d43acaf0ed717e25e7086cb0b74509c
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2015-06-13 00:43:38 +0300

    PEP-8

--- src/point.py
+++ src/point.py
@@ -1,8 +1,9 @@
 # Copyright (c) Alex Ponomarev.
 # Distributed under the terms of the MIT License.
 
+
 class point:
-    #Class's constructor
+    # Class's constructor
     def __init__(self, x, y):
         self.x = x
         self.y = y
