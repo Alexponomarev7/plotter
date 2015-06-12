422bce01d3f3a5d8ac759261030ad620816f4eb0
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2015-06-12 23:47:51 +0300

    PEP-8

--- src/drawing.py
+++ src/drawing.py
@@ -9,13 +9,14 @@ CENTER = HEIGHT // 2
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-#drawing GIF image
+
+# drawing GIF image
 def draw(points, name):
     # PIL create an empty image and draw object to draw on
     # memory only, not visible
     image = Image.new("RGB", (WIDTH, HEIGHT), WHITE)
     draw = ImageDraw.Draw(image)
-    
+
     for i in range(len(points) - 1):
         # do the PIL image/draw (in memory) drawings
         draw.line([points[i].x + WIDTH // 2,
@@ -26,7 +27,3 @@ def draw(points, name):
     # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
     filename = name + ".gif"
     image.save(filename)
-    
-    
-    
-    
