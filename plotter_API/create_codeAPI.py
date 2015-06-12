7151be7a4cba2e5c13687abb3e6d3deddd889dc6
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2015-06-13 00:48:57 +0300

    PEP-8

--- plotter_API/create_codeAPI.py
+++ plotter_API/create_codeAPI.py
@@ -1,12 +1,12 @@
 # Copyright (c) Alex Ponomarev.
 # Distributed under the terms of the MIT License.
 
+
 def writingcode(commands, path):
-    f_w = open('w', path + "output.pl")
-    
+    f_w = open(path + "output.pl", 'w')
+
     print('pencilUp()', file=f_w)
     for i in commands:
         print(str(i), file=f_w)
-    
+
     f_w.close()
-    
