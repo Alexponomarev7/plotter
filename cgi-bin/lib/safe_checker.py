5ce72a5620689f3e3fbd738edf9e4a381dc28c2b
ogorodnikoff2012 <ogorodnikoff2012@yandex.ru>
2015-07-09 15:09:02 +0300

    Bug fixes

--- cgi-bin/lib/safe_checker.py
+++ cgi-bin/lib/safe_checker.py
@@ -4,7 +4,7 @@ safe_tokens = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 '+', '-', '*', '/',
                 '(', ')', '[', ']', 
                 'sin', 'cos', 'tg', 'ctg', 'arcsin', 'arccos', 'arctg', 'arcctg', 'f', 'pi', 'x', 'sgn',
-                ' ']
+                ' ', '.']
 
 class SafetyException(Exception):
     pass
@@ -13,5 +13,5 @@ def check(expr):
     for token in safe_tokens:
         expr = expr.replace(token, '')
     if len(expr) != 0:
-        raise SafetyException()
+        raise SafetyException(expr)
 
